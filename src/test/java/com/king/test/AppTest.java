1b8ee30d2024d8a7a6ba071ac8ca1029fc3bbb13
1277082377@qq.com <huajie2552@163.com>
2020-01-04 16:04:38 +0800

    test

--- src/test/java/com/king/test/AppTest.java
+++ src/test/java/com/king/test/AppTest.java
@@ -16,6 +16,6 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-        System.out.println("After Junit Case!");
+        System.out.println("After Junit Case1111!");
     }
 }
